257360e451aaf0fd9d34d9736fcd223742400805
Thomas <tchoquet@student.42tokyo.jp>
2023-07-29 13:05:29 +0900

    environement variables

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 20:57:41 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/07/23 23:26:07 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/07/27 16:07:17 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,10 @@
 
 # include <libft.h>
 # include "external_functions.h"
+
+int		init_env(char *envp[]);
+int		set_env(const char *key, const char *val);
+char	*get_env(const char *key);
+void	clean_env(void);
 
 #endif // MINISHELL_H
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 20:57:27 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/07/24 02:45:41 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/07/27 17:43:37 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ static void	destructor(void)
 	char	*pid;
 	char	*cmd;
 
-	// print_reporjt();
+	print_report();
 	pid = ft_itoa(getpid());
 	cmd = ft_strjoin("leaks -q ", pid);
-	// system((const char *)cmd);
+	system((const char *)cmd);
 	free(pid);
 	free(cmd);
 }
@@ -32,16 +32,40 @@ static void	destructor(void)
 
 int	main(int argc, char *argv[], char *envp[])
 {
-	char *env[] = {"PATH=Bite", NULL};
-	execve("print_env", NULL, env);
-	return (errno);
+	char	*cmd;
+	char	*var;
+	char	*splited_str[2];
+
+	if (init_env(envp) != 0)
+		return (1);
+
+	cmd = NULL;
+	while (1)
+	{
+		cmd = readline("minishell > ");
+		if (ft_strncmp(cmd, "exit", 4) == 0)
+			break ;
+		if (ft_strchr(cmd, '=') == NULL)
+		{
+			var = get_env((const char *)cmd);
+			ft_printf("%s\n", var);
+			free(var);
+			free(cmd);
+			continue ;
+		}
+		splited_str[0] = ft_substr(cmd, 0, ft_strchr(cmd, '=') - cmd);
+		splited_str[1] = ft_strdup(ft_strchr(cmd, '=') + 1);	
+		if (splited_str[0] == NULL || splited_str[1] == NULL || set_env(splited_str[0], splited_str[1]) != 0)
+		{
+			free(splited_str[0]);
+			free(splited_str[1]);
+			break ;
+		}
+		free(splited_str[0]);
+		free(splited_str[1]);
+		free(cmd);
+	}
+	free(cmd);
+	clean_env();
+	return (0);
 }
-
-// int	main(int argc, char *argv[], char *envp[])
-// {
-// 	for (size_t i = 0; envp[i] != NULL; i++)
-// 		ft_printf("%s\n", envp[i]);
-// 	ft_printf("\n\n%s\n", getenv("PATH"));
-
-// 	return (0);
-// }
