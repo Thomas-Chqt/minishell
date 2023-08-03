b73ce6241621c0cd7df1914d80ee996fa2f6baba
jizots <hotphilly1776@yahoo.co.jp>
2023-08-03 16:42:39 +0900

    Add close for old fd & move in fork func

--- sources/exec_scan_btree.c
+++ sources/exec_scan_btree.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_exec_scan_btree.c                               :+:      :+:    :+:   */
+/*   exec_scan_btree.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 21:54:41 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/08/03 11:05:00 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/08/03 16:21:39 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,17 @@ int	ft_readline(char fd_pipe[2], char *delimiter)
 	exit(0);
 }
 
-int	ft_here_doc(char *delimiter)
+int	ft_here_doc(char *delimiter, int fd_in)
 {
 	int		fd_pipe[2];
 	int		pid;
 	char	*line;
 
+	if (fd_in > 0)
+	{
+		if (close(fd_in) == -1)
+			exit(ft_print_perror("Error. Fail to close pipe.\n"));
+	}
 	if (pipe(fd_pipe) == -1)
 		exit(ft_print_perror("Error. Fail to create pipe.\n"));
 	pid = fork();
@@ -54,69 +59,77 @@ int	ft_here_doc(char *delimiter)
 		ft_readline(fd_pipe, delimiter);
 	else
 	{
-		waitpid(pid, NULL, 0);//wait for child process
+		waitpid(pid, NULL, 0);
 		if (close(fd_pipe[1]) == -1)
 			exit(ft_print_perror("Error. Fail to close pipe.\n"));
 	}
 	return (fd_pipe[0]);
 }
 
-int	scan_btree_fd(t_dexec *dexec, t_tree *node)
+int	scan_btree_fd(t_dexec *dexec, t_syntree *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->type == LESS)
-		dexec->fd_in = ft_open_file(node->left->data, LESS);
-	else if (node->type == GREAT)
-		dexec->fd_out = ft_open_file(node->left->data, GREAT);
-	else if (node->type == DGREAT)
-		dexec->fd_out = ft_open_file(node->left->data, DGREAT);
-	else if (node->type == DLESS)
-		dexec->fd_in = ft_here_doc(node->left->data);
+	if (node->data->type == LESS)
+		dexec->fd_in = ft_open_file(node->left->data, LESS, dexec->fd_in);
+	else if (node->data->type == GREAT)
+		dexec->fd_out = ft_open_file(node->left->data, GREAT, dexec->fd_out);
+	else if (node->data->type == DGREAT)
+		dexec->fd_out = ft_open_file(node->left->data, DGREAT, dexec->fd_out);
+	else if (node->data->type == DLESS)
+		dexec->fd_in = ft_here_doc(node->left->data->data, dexec->fd_in);
 	scan_btree_fd(dexec, node->right);
 	return (0);
 }
 
-int	scan_btree_cmd(int fd_in, int fd_out, t_tree *node)
+int	scan_btree_cmd(int fd_in, int fd_out, t_syntree *node)
 {
 	t_dexec	dexec;
+	int		pid;
+	int		status;
 
 	if (node == NULL)
 		return (0);
 	dexec.fd_in = fd_in;
 	dexec.fd_out = fd_out;
-	scan_btree_fd(&dexec, node->left);
-	if (node->type == NODE_CMD)
+	if (node->data->type == TEXT)
 	{
-		dexec.cmd_opts = //joint cmd & opts to char** matrix, end with NULL;
-		if (dexec.cmd_opts == NULL)
-			exit(ft_mes_error("Error. Fail allocate memory.\n"));
-		ft_exec_fork(path, prog, cmd_opts, envp, &dexec);//Draft version
+		pid = fork();
+		if (pid == -1)
+			exit(ft_print_perror("Error. Fail to fork.\n"));
+		else if (pid == 0)
+			ft_exec_forked(&dexec, node);
+		else
+		{
+			;//wait? for get exit#
+		}
 	}
 	return (0);
 }
 
 //first call: scan_btree_pipe(0, 1, node);
-int	scan_btree_pipe(int fd_in, int fd_out, t_tree *node)
+int	scan_btree_pipe(int fd_in, int fd_out, t_syntree *node)
 {
 	int	fd_pipe[2];
 
 	if (node == NULL)
 		return (0);
-	if (node->type == NODE_PIPE)
+	if (node->data->type == PIPE)
 	{
 		if (pipe(fd_pipe) == -1)
 			exit(ft_print_perror("Error. Fail to create pipe.\n"));
 		if (scan_btree(fd_in, fd_pipe[1], node->left) == 1)
 			return (1);
 		if (close(fd_pipe[1]) == -1)
-			exit(ft_print_perror("Error. Fail to close pipe.\n"));
+			exit(ft_print_perror("Error. Fail to close pipe[1].\n"));
 		if (scan_btree(fd_pipe[0], fd_out, node->right) == 1)
 			return (1);
 	}
 	else
 	{
 		scan_btree_cmd(fd_in, fd_out, node);
+		if (close(fd_pipe[0]) == -1)
+			exit(ft_print_perror("Error. Fail to close pipe[0].\n"));
 	}
 	return (0);
 }
