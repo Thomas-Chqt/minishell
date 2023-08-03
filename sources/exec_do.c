a06f4f74ffe670a28cad2285db028d4afbc599f9
jizots <hotphilly1776@yahoo.co.jp>
2023-08-03 16:23:48 +0900

    move redirect func

--- sources/exec_do.c
+++ sources/exec_do.c
@@ -6,34 +6,50 @@
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 13:32:49 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/08/03 11:57:03 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/08/03 16:22:51 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+int	ft_exec_set_redirect(t_dexec *dexec)
+{
+	if (dexec->fd_in != STDIN_FILENO)
+	{
+		if (dup2(dexec->fd_in, STDIN_FILENO) == -1)
+			exit(ft_print_perror("Error. Fail to dup2_fd_in.\n"));
+		if (close(dexec->fd_in) == -1)
+			exit(ft_print_perror("Error. Fail to close_fd_in.\n"));
+	}
+	if (dexec->fd_out != STDOUT_FILENO)
+	{
+		if (dup2(dexec->fd_out, STDOUT_FILENO) == -1)
+			exit(ft_print_perror("Error. Fail to dup2_fd_out.\n"));
+		if (close(dexec->fd_out) == -1)
+			exit(ft_print_perror("Error. Fail to close_fd_out.\n"));
+	}
+	return (0);
+}
 
 int	ft_exec_do_cmd(char *cmd_path, char **cmd_opts, char **envp)
 {
 	if (execve(cmd_path, cmd_opts, envp) == -1)
 	{
 		if (errno == EISDIR)
-			cmd_cant_use(cmd_path, CMD_SIMPLE);
+			cmd_cant_use(cmd_path, IS_A_DIRECTORY);
 		exit(1);
 	}
 }
 
-int	ft_exec_forked(t_dexec *dexec, t_tree *node)
+int	ft_exec_forked(t_dexec *dexec, t_syntree *node)
 {
 	if (node->left != NULL)
 		scan_btree_fd(dexec, node->left);
-	if (node->type == NODE_CMD)
+	ft_exec_set_redirect(dexec);
+	if (node->data->type == TEXT && node->data->data != NULL)
 	{
-		dexec.cmd_opts = //joint cmd & opts to char** matrix, end with NULL;
-		if (dexec.cmd_opts == NULL)
-			exit(ft_mes_error("Error. Fail allocate memory.\n"));
-		dexec.matrix_envpath =  ft_split_by_token(dexec.matrix_envpath, ':');
-		dexec.cmd_path = ft_get_cmdpath(path, prog, &dexec);
-		ft_exec_do_cmd(dexec.cmd_path, cmd_opts, envp);
+		//Get data of cmd_path, cmd_opts, envp is not available.
+		ft_exec_do_cmd(dexec->cmd_path, dexec->cmd_opts, envp);
 	}
 	return (0);
 }
