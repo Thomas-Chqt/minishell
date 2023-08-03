7508506a5342877290ac6ffbc490ad0cfee88836
jizots <hotphilly1776@yahoo.co.jp>
2023-08-03 12:01:53 +0900

    fork function removed

--- sources/exec_do.c
+++ sources/exec_do.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_exec_do.c                                       :+:      :+:    :+:   */
+/*   exec_do.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 13:32:49 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/08/02 22:01:49 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/08/03 11:57:03 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,18 @@ int	ft_exec_do_cmd(char *cmd_path, char **cmd_opts, char **envp)
 	}
 }
 
-int	ft_exec_fork(char *path, char *prog, char **cmd_opts, char **envp)
+int	ft_exec_forked(t_dexec *dexec, t_tree *node)
 {
-	t_pid	pid;
-	int		status;
-
-	pid = fork();
-	if (pid == -1)
-		exit(ft_print_perror("Error. Fail to fork.\n"));
-	else if (pid == 0)
+	if (node->left != NULL)
+		scan_btree_fd(dexec, node->left);
+	if (node->type == NODE_CMD)
 	{
+		dexec.cmd_opts = //joint cmd & opts to char** matrix, end with NULL;
+		if (dexec.cmd_opts == NULL)
+			exit(ft_mes_error("Error. Fail allocate memory.\n"));
 		dexec.matrix_envpath =  ft_split_by_token(dexec.matrix_envpath, ':');
 		dexec.cmd_path = ft_get_cmdpath(path, prog, &dexec);
 		ft_exec_do_cmd(dexec.cmd_path, cmd_opts, envp);
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
 	}
 	return (0);
 }
