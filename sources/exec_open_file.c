a68088f12105da296ae84e4f1eb7d7193fcabbd4
jizots <hotphilly1776@yahoo.co.jp>
2023-08-03 16:40:27 +0900

    add close for old fd

--- sources/exec_open_file.c
+++ sources/exec_open_file.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_exec_open_file.c                                :+:      :+:    :+:   */
+/*   exec_open_file.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 18:57:15 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/08/02 22:08:39 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/08/03 16:38:37 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-int	ft_open_file(char *file_redirect, int flag_redirect)
+int	ft_open_file(char *file_redirect, int flag_redirect, int fd_io)
 {
 	int	fd;
 
@@ -27,6 +27,14 @@ int	ft_open_file(char *file_redirect, int flag_redirect)
 		ft_printf("minishell: ");
 		ft_print_perror(file_redirect);
 		exit (EX_FILE_OPEN_ERR);
+	}
+	else
+	{
+		if (fd_io != fd && fd_io != STDIN_FILENO && fd_io != STDOUT_FILENO)
+		{
+			if (close(fd_io) == -1)
+				exit(ft_print_perror("Error. Fail to close.\n"));
+		}
 	}
 	return (fd);
 }
