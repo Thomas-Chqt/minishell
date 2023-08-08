2cbdc7379cdc20ad4a457084c05722bd12dd91c8
Thomas <semoir.dense-0h@icloud.com>
2023-08-08 18:24:10 +0900

    revers last 3 commits

---
+++ includes/error_codes.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_codes.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 15:30:04 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/08/04 18:14:17 by sotanaka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_CODES_H
+# define ERROR_CODES_H
+
+# define MALLOC_ERROR 1111
+# define MISSING_QUOTE 2222
+
+#endif // ERROR_CODES_H
---
+++ sources/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hotph <hotph@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/23 20:57:27 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/08/08 14:53:43 by hotph            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+#ifdef MEMCHECK
+__attribute__((destructor))
+static void	destructor(void)
+{
+	char	*pid;
+	char	*cmd;
+
+	print_report();
+	pid = ft_itoa(getpid());
+	cmd = ft_strjoin("leaks -q ", pid);
+	system((const char *)cmd);
+	free(pid);
+	free(cmd);
+}
+
+#endif // MEMCHECK
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	if (init_env(envp) != 0)
+		return (1);
+	minishell_loop();
+	clean_env();
+	return (0);
+}
