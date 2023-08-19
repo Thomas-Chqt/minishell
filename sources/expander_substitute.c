cfc2e6f3102d0165a50e1d7c60471bcf2b1cdd50
Thomas <semoir.dense-0h@icloud.com>
2023-08-19 13:29:16 +0900

    ..

--- sources/expander_substitute.c
+++ sources/expander_substitute.c
@@ -1,32 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expander.h                                         :+:      :+:    :+:   */
+/*   expander_substitute.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/09 21:29:07 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/08/18 22:21:36 by tchoquet         ###   ########.fr       */
+/*   Created: 2023/08/18 22:21:14 by tchoquet          #+#    #+#             */
+/*   Updated: 2023/08/18 22:35:38 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXPANDER_H
-# define EXPANDER_H
+#include "minishell.h"
 
-# include "minishell.h"
-# include "error.h"
-# include "lexer.h"
-# include "parser.h"
-# include "environment.h"
-
-typedef struct s_str_list	t_str_list;
-
-struct s_str_list
-{
-	char		*str;
-	t_str_list	*next;
-};
-
-int	expand_ast(t_ast *ast);
-
-#endif // EXPANDER_H
+int	add_substitued(char	*str, )
