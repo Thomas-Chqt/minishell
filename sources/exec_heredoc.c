f5549041f1185d16c9aa12447eac0d36073a5b21
Thomas <semoir.dense-0h@icloud.com>
2023-08-27 19:51:46 +0900

    Thomas-Chqt&Jizots/issue98 (#116)
    Add expander in here doc

--- sources/exec_heredoc.c
+++ sources/exec_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 13:24:07 by sotanaka          #+#    #+#             */
-/*   Updated: 2023/08/26 11:03:19 by sotanaka         ###   ########.fr       */
+/*   Updated: 2023/08/27 19:30:42 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,42 @@ char	*get_temporary_name10(char *namespace, char *name, int size_space)
 	return (namespace);
 }
 
-static int	ft_readline(int *fd_tmphere, char *delimiter)
+static int	here_doc_expand(char *filename_in, size_t len_here)
+{
+	int		fd_in;
+	int		fd_out;
+	int		status;
+	char	filename_out[15];
+
+	fd_in = STDIN_FILENO;
+	if (ft_open_file(filename_in, LESS, &fd_in) == 1)
+		perror_wrap("heredoc expand", 1);
+	fd_out = STDOUT_FILENO;
+	if (ft_open_file(get_temporary_name10
+			(filename_out, ".TMPminishell", 15), GREAT, &fd_out) == 1)
+		return (1);
+	status = put_expanded_file_fd(fd_in, len_here, fd_out);
+	if (close(fd_out) == -1)
+		perror_wrap("close: here_doc expand", 1);
+	if (status == 1)
+		return (status);
+	return (print_error(status));
+}
+
+static void	putstr_to_fd(char *line, int *fd)
+{
+	if (*line == '\0')
+		ft_putstr_fd("\n", *fd);
+	else
+		ft_putendl_fd(line, *fd);
+}
+
+static int	ft_readline(int *fd_tmphere, char *delimiter, char *filename)
 {
 	char	*line;
+	size_t	len;
 
+	len = 0;
 	if (sig_heredoc_mode() != 0)
 		return (print_error(SIGACTION_ERROR));
 	while (1)
@@ -44,12 +76,12 @@ static int	ft_readline(int *fd_tmphere, char *delimiter)
 				exit(perror_wrap("readline", 1));
 			break ;
 		}
-		if (*line == '\0')
-			ft_putstr_fd("\n", *fd_tmphere);
-		else
-			ft_putendl_fd(line, *fd_tmphere);
+		len += ft_strlen(line) + 1;
+		putstr_to_fd(line, fd_tmphere);
 		free(line);
 	}
+	if (here_doc_expand(filename, len) != 0)
+		exit (1);
 	exit(0);
 }
 
@@ -57,23 +89,24 @@ int	ft_here_doc(char *delimiter, int *fd_in)
 {
 	int		pid;
 	int		status;
-	char	filename[15];
+	char	filename1[15];
 
 	if (sig_heredoc_parent_mode() != 0)
 		return (print_error(SIGACTION_ERROR));
 	if (ft_open_file(get_temporary_name10
-			(filename, ".TMPminishell", 15), GREAT, fd_in) == 1)
+			(filename1, ".TMPminishell", 15), GREAT, fd_in) == 1)
 		return (1);
 	pid = fork();
 	if (pid == -1)
 		return (perror_wrap("here_doc fork", 1));
 	if (pid == 0)
-		ft_readline(fd_in, delimiter);
+		ft_readline(fd_in, delimiter, filename1);
 	else
 	{
 		if (waitpid(pid, &status, 0) == -1 || sig_forwarding_mode() != 0)
 			return (perror_wrap("here_doc", 1));
-		if (ft_open_file(filename, LESS, fd_in) == 1)
+		if (ft_open_file(get_temporary_name10
+				(filename1, ".TMPminishell", 15), LESS, fd_in) == 1)
 			return (1);
 		if (WIFEXITED(status))
 			return (WEXITSTATUS(status));
--- sources/expander.c
+++ sources/expander.c
@@ -3,22 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   expander.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: sotanaka <sotanaka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 17:06:42 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/08/27 14:02:19 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/08/27 19:24:57 by sotanaka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expander.h"
 
-t_pre_toklist	*make_pre_toklist(t_token token, int *error);
+t_pre_toklist		*make_pre_toklist(t_token token, int *error);
 
-int				add_retokenized_pre_token(t_pre_token pre_token,
-					t_toklist **token_list);
+int					add_retokenized_pre_token(t_pre_token pre_token,
+						t_toklist **token_list);
 
-static int		add_expanded_token(t_token token, t_toklist **token_list);
-static int		add_duped_token(t_token token, t_toklist **token_list);
+static int			add_expanded_token(t_token token, t_toklist **token_list);
+static int			add_duped_token(t_token token, t_toklist **token_list);
+static t_toklist	*token_from_file(int fd, t_uint64 len);
 
 int	expand_toklist(t_toklist **token_list)
 {
@@ -46,6 +47,21 @@ int	expand_toklist(t_toklist **token_list)
 		*token_list = expanded_toklist;
 	if (error != 1)
 		return (print_error(error));
+	return (error);
+}
+
+int	put_expanded_file_fd(int fd_in, size_t len, int fd_out)
+{
+	t_toklist	*temp_token;
+	int			error;
+
+	temp_token = token_from_file(fd_in, len);
+	if (temp_token == NULL)
+		return (MALLOC_ERROR);
+	error = expand_toklist(&temp_token);
+	if (error == 0)
+		ft_putstr_fd(temp_token->data->data, fd_out);
+	ft_lstclear((t_list **)&temp_token, &free_token);
 	return (error);
 }
 
@@ -86,4 +102,27 @@ static int	add_duped_token(t_token token, t_toklist **token_list)
 		return (MALLOC_ERROR);
 	ft_lstadd_back((t_list **)token_list, (t_list *)duped_token);
 	return (0);
+}
+
+static t_toklist	*token_from_file(int fd, t_uint64 len)
+{
+	t_toklist	*token;
+	char		*file_data;
+	char		*line;
+
+	file_data = ft_calloc(len + 1, sizeof(char));
+	if (file_data == NULL)
+		return (NULL);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		ft_strlcat(file_data, line, len + 1);
+		free(line);
+		line = get_next_line(fd);
+	}
+	token = toklist_new(DQUOTED, file_data);
+	if (token != NULL)
+		return (token);
+	free(file_data);
+	return (NULL);
 }
